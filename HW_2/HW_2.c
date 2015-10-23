e9ca0b5d9326320975827ea14d1c99883766d4be
2ZeroSix <2ZeroSix1@gmail.com>
2015-10-23 10:22:23 +0600

    HW_2 update
    encoder

--- HW_2/HW_2.c
+++ HW_2/HW_2.c
@@ -4,6 +4,45 @@
 #define decode "-d"
 #define ignore "-i"
 /*
+Text to Base64
+Input
+-input file <*in>
+-output file <*out>
+Changes
+-print Base64 code in <*out>
+*/
+void encoder(FILE *in, FILE *out)
+{
+	char c[3];
+	// char b64[4];
+	while(c[0] = getc(in) != EOF)
+	{
+		printf("%c", c[0] >> 2);
+		if((c[1] = getc(in)) == EOF)
+		{
+			c[1] = 0;
+			printf("%c", ((c[0] << 4) | (c[1] >> 4)) | 0x3f);
+			return;
+		}
+		printf("%c", ((c[0] << 4) | (c[1] >> 4)) | 0x3f);
+		if((c[2] = getc(in)) == EOF)
+		{
+			c[2] = 0;
+			printf("%c", ((c[1] << 2) | c[2] >> 6) | 0x3f);
+			return;
+		}
+		printf("%c", ((c[1] << 2) | c[2] >> 6) | 0x3f);
+		printf("%c", (c[2]) | 0x3f);
+		// b64[0] = c[0] >> 2
+		// b64[1] = ((c[0] << 4) | (c[1] >> 4)) | 0x3f;
+		// b64[2] = ((c[1] << 2) | c[2] >> 6) | 0x3f;
+		// b64[3] = (c[2]) | 0x3f;
+	}
+	return;
+}
+/*
+Base64 to text*/
+/*
 input
 -count of arguments <argc>
 -arguments <**argv>
@@ -35,34 +74,42 @@ char checkmode(int argc, char **argv, FILE *in, FILE *out, char *check)
 		i++;
 		if (strcmp(argv[i], encode) == 0)
 			check[i - 1] = 'e';
-		checkall *= check[i - 1];
 		if (strcmp(argv[i++], decode) == 0)
 			check[i - 2] = 'd';
 		checkall *= check[i - 2];
-		if(in = fopen(argv[i++], "w")
+		if((in = fopen(argv[i++], "r") == NULL)
 			check[i - 2] = 1;
 		checkall *= check[i - 2];
-		out = fopen(argv[i])
+		if((out = fopen(argv[i], "w")) == NULL)
+			check[i - 1] = 1;
+			checkall *= check[i - 1];
 	}
 	if (checkall)
 		return 1;
 	return 0;
 }
 
+/*
+Print outcode
+Input
+-checks <*check>
+-count of arguments <argc>
+-argumentts <**argv>
+*/
 void output(char *check, int argc, char **argv)
 {
-	char outcode = {"wrong mode: ", "Wrong mode: ", "Wrong input file: ", }
+	char outcode[] = {"wrong ignore mode: ", "Wrong decode/encode mode: ", "Wrong input file: ", "COMPLETEED\nResult in output file", "ERROR\nInterface: <ignore mode> <decode/encode mode> <input file> <output file>\nIgnore modes:\n- <-i> - ignore nonBase64 symbols while decoding\n- empty - not ignore nonBase64 symbols\nDecode/encode modes:\n- <-d> - decode\n- <-e> - encode\nInput file\nOutput file\n"}
 	short i = 0;
 	int checkall = 1;
 	if (argc == 4)
 	{
 		if (check[i] == 0)
 			printf("%s %s\n", outcode[i], argv[i + 1]);
-		checkall *= check[i++]
+		checkall *= check[i++];
 	}
 	if (check[i] == 0)
 		printf("%s %s\n", outcode[i], argv[i + 1]);
-	checkall *= check[i++]
+	checkall *= check[i++];
 	if (check[i] == 0)
 		printf("%s %s\n", outcode[i], argv[i + 1]);
 	checkall *=check[i++];
@@ -70,7 +117,13 @@ void output(char *check, int argc, char **argv)
 		printf("%s %s\n", outcode[i], argv[i + 1]);
 	checkall *=check[i++];
 	if (checkall)
-		printf("%s\n", outcode[i])
+	{
+		printf("%s\n", outcode[i]);
+	}
+	else
+	{
+		printf("%s\n" outcode[i + 1]);
+	}
 	return;
 }
 
@@ -80,7 +133,14 @@ void main(int argc, char **argv)
 	FILE *in, *out;
 	if(checkmode(argc, argv, in, out, check))
 	{
-
+		if(check[2] == encode)
+		{
+			encoder(in, out);
+		}
+		else
+		{
+			decoder(in, out, check[1]);
+		}
 	}
 	output(check, argc, argv);
 	return;
