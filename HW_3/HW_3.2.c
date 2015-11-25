3dab4a9d268e5d0bc90ed2c5e0febe2b0baeaa6b
2ZeroSix <2ZeroSix1@gmail.com>
2015-11-25 12:09:41 +0600

    complete HW_3.2

--- HW_3/HW_3.2.c
+++ HW_3/HW_3.2.c
@@ -55,6 +55,7 @@ int sortage(const student* std1, const student* std2)
 void printfsort(student* studmas, FILE *out, int count)
 {
 	int i;
+	printf("output\n");
 	for (i = 0; i < count; i++)
 	{
 		fprintf(out, "%s %.2f %d\n", studmas[i].name, studmas[i].avrate, studmas[i].age);
@@ -63,15 +64,17 @@ void printfsort(student* studmas, FILE *out, int count)
 
 void sortandprint(FILE *out1, FILE *out2, FILE *out3, student* studmas, int count)
 {
-	FILE *checkmas = fopen("checkmas.txt", "w");
-	printfsort(studmas, checkmas, count);
-	fclose(checkmas);
-	qsort(studmas, count, sizeof(student*), (int(*) (const void *, const void *)) sortname);
+	printf("sort and print in files\n");
+	qsort(studmas, count, sizeof(student), (int(*) (const void *, const void *)) sortname);
+	printf("\tfirst sort -> ");
 	printfsort(studmas, out1, count);
-	qsort(studmas, count, sizeof(student*), (int(*) (const void *, const void *)) sortavrate);
+	qsort(studmas, count, sizeof(student), (int(*) (const void *, const void *)) sortavrate);
+	printf("\tsecond sort -> ");
 	printfsort(studmas, out2, count);
-	qsort(studmas, count, sizeof(student*), (int(*) (const void *, const void *)) sortage);
+	qsort(studmas, count, sizeof(student), (int(*) (const void *, const void *)) sortage);
+	printf("\tthird sort -> ");
 	printfsort(studmas, out3, count);
+	printf("Complete sort\n");
 	return;
 }
 
@@ -80,6 +83,7 @@ char rstudlist(FILE *in, FILE *out1, FILE *out2, FILE *out3, student **studmas)
 	int i, count = 0;
 	char c;
 	student *temp, *stm, *Head = NULL;
+	printf("reading input file:\n");
 	while((c = getc(in)) != EOF)
 	{
 		student *newst = (student*)malloc(sizeof(student));
@@ -95,10 +99,10 @@ char rstudlist(FILE *in, FILE *out1, FILE *out2, FILE *out3, student **studmas)
 		fscanf(in, "%f %d", &(newst->avrate), &(newst->age));
 		pushlist(newst, &Head);
 		getc(in);
-		printf("%d: %s %f %d\n", count, newst->name, newst->avrate, newst->age);
+		printf("\t%d: %s %.2f %d\n", count, newst->name, newst->avrate, newst->age);
 		count++;
 	}
-	stm = *studmas = (student*) malloc (sizeof(student)*count);
+	stm = *studmas = (student*) malloc ((sizeof(student)+sizeof(char)*20)*count);
 	i = 1;
 	while ((temp = poplist(&Head))!= NULL)
 	{
@@ -106,20 +110,28 @@ char rstudlist(FILE *in, FILE *out1, FILE *out2, FILE *out3, student **studmas)
 		stm[count - i].age = temp->age;
 		stm[count - i].avrate = temp->avrate;
 		i++;
+		free(temp);
 	}
+	printf("\tCount: %d\nComplete read\n", count);
 	return count;
 }
 
 int open(FILE **in, FILE **out1, FILE **out2, FILE **out3)
 {
+	printf("Open files\n");
 	if((*in = fopen("in.txt", "r")) == NULL)
  		return 0;
+ 	printf("\tin\n");
 	if((*out1 = fopen("out1.txt", "w")) == NULL)
 		return 1;
+	printf("\tout1\n");
 	if((*out2 = fopen("out2.txt", "w")) == NULL)
 		return 2;
+	printf("\tout2\n");
 	if((*out3 = fopen("out3.txt", "w")) == NULL)
 		return 3;
+	printf("\tout3\n");
+	printf("complete open\n");
 	return 4;
 }
 
@@ -128,15 +140,17 @@ void main()
 	FILE *in, *out1, *out2, *out3;
 	student *studmas;
 	int count, check;
+	printf("Protocol:\n");
 	if((check = open(&in, &out1, &out2, &out3)) == 4)
 	{
 		count = rstudlist(in, out1, out2, out3, &studmas);
 		sortandprint(out1, out2, out3, studmas, count);
 		free(studmas);
+		printf("Complete all\n");
 	}
 	else
 	{
-		printf("wrong files");
+		printf("ERROR:");
 	}
 	if (check > 0)
 		fclose(in);
--- HW_3/try.c
+++ HW_3/try.c
@@ -39,6 +39,7 @@ int sortage(const void* std1, const void* std2)
 void printfsort(student* studmas, int count)
 {
 	int i;
+	printf("sort\n");
 	for (i = 0; i < count; i++)
 	{
 		printf("%s %f %d\n", studmas[i].name, studmas[i].avrate, studmas[i].age);
@@ -49,10 +50,18 @@ void main()
 {
 	student arr[5];
 	int i;
+	char c;
 	for(i = 0; i < 5; i++)
 	{
-	scanf("%s %f %d", arr[i].name, &(arr[i].avrate), &(arr[i].age));
-	printf("%s %f %d", arr[i].name, arr[i].avrate, arr[i].age);
+	int j = 0;
+	while((c = getchar()) != ' ') 	
+	{
+		arr[i].name[j++] = c;
+	}
+		arr[i].name[j] = '\0';
+	scanf("%f %d", &(arr[i].avrate), &(arr[i].age));
+	printf("%s %f %d\n", arr[i].name, arr[i].avrate, arr[i].age);
+	getchar();
 	}
 	qsort(arr, 5, sizeof(student), sortname);
 	printfsort(arr, 5);
@@ -60,4 +69,5 @@ void main()
 	printfsort(arr, 5);
 	qsort(arr, 5, sizeof(student), sortage);
 	printfsort(arr, 5);
+	getchar();
 }
