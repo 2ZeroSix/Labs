bbf76a2601d9bc6f52e50caed924d5e6a3ccc386
2ZeroSix <2ZeroSix1@gmail.com>
2015-12-06 18:43:28 +0600

    start unlimited calculator

---
+++ lab7C_complete/testcalc.c
@@ -0,0 +1,8 @@
+#include "unlimnums.h"
+#include <stdio.h>
+
+void main()
+{
+	printf("%s\n", ba_add("157\0", "3\0"));
+	return;
+}
---
+++ lab7C_complete/unlimnums.h
@@ -0,0 +1,18 @@
+#ifndef _UNLIMNUMS_H_
+#define _UNLIMNUMS_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+char* ba_add(char* arg1, char* arg2);
+
+char* ba_sub(char* arg1, char* arg2);
+
+char* ba_mul(char* arg1, char* arg2);
+
+char* ba_div(char* arg1, char* arg2);
+
+char* ba_mod(char* arg1, char* arg2);
+
+#endif
