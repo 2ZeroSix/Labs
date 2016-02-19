cd0025a0cec6ff6001cbd9ec0a065026e21b1185
2ZeroSix_DEXP <2ZeroSix1@gmail.com>
2016-02-19 10:23:20 +0600

    разбил на файлы, переимаеновал функции поворотов

--- semester_2/lab2_AVL_complete/avl_func.c
+++ semester_2/lab2_AVL_complete/avl_func.c
@@ -1,41 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include "avl_func.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define type_tree int // тип значений элементов дерева
-typedef struct _avl_tree{
-	unsigned char height;
-	type_tree val;
-	struct _avl_tree* left;
-	struct _avl_tree* right;
-}avl_tree;
-
-/**
- * наибольший из данных элементов unsigned char
- * @param  a
- * @param  b
- * @return наибольший
- * */
 unsigned char maxuchar(unsigned char a, unsigned char b) {
 	return (a > b) ? a : b;
 }
 
-/**
- * определение высоты дерева по высотам его поддеревьев
- * @param  root ненулевой указатель на дерево
- * @return      высота дерева (верна только если высоты поддеревьев верны)
- */
 unsigned char height_avl(avl_tree* root) {
 	return maxuchar(((root->left) ? root->left->height : 0), ((root->right) ? root->right->height : 0)) + 1;
 }
 
-/**
- * малое правое вращение
- * @param  root ненулевой указатель на дерево
- * @return      указатель на дерево после вращения
- */
-avl_tree* right (avl_tree* root) {
+avl_tree* avl_right (avl_tree* root) {
 	avl_tree* b = root->left;
 	root->left = b->right;
 	b->right = root;
@@ -44,12 +18,7 @@ avl_tree* right (avl_tree* root) {
 	return b;
 }
 
-/**
- * малое левое вращение
- * @param  root ненулевой указатель на дерево
- * @return      указатель на дерево после вращения
- */
-avl_tree* left (avl_tree* root) {
+avl_tree* avl_left (avl_tree* root) {
 	avl_tree* b = root->right;
 	root->right = b->left;
 	b->left = root;
@@ -58,49 +27,34 @@ avl_tree* left (avl_tree* root) {
 	return b;
 }
 
-/**
- * Большое правое вращение
- * @param  root ненулевой указатель на дерево
- * @return      указатель на дерево после вращения
- */
-avl_tree* big_right (avl_tree* root) {
+avl_tree* avl_big_right (avl_tree* root) {
 	avl_tree* b = root->left;
-	root->left = left(b);
-	return right(root);
+	root->left = avl_left(b);
+	return avl_right(root);
 }
 
-/**
- * Большое левое вращение
- * @param  root ненулевой указатель на дерево
- * @return      указатель на дерево после вращения
- */
-avl_tree* big_left (avl_tree* root) {
+avl_tree* avl_big_left (avl_tree* root) {
 	avl_tree* b = root->right;
-	root->right = right(b);
-	return left(root);
+	root->right = avl_right(b);
+	return avl_left(root);
 }
 
-/**
- * Определение необходимого вращения, вращение и изменение высоты на корректное
- * @param  root ненулевой указатель на дерево
- * @return      указатель на дерево после вращения
- */
 avl_tree* avl_rotation (avl_tree* root) {
 	switch(((root->left) ? root->left->height : 0) - ((root->right) ? root->right->height : 0)) {
 		case 2:
 			if (((root->left->left) ? root->left->left->height : 0) >= ((root->left->right) ? root->left->right->height : 0)) {
-				root = right(root);
+				root = avl_right(root);
 			}
 			else {
-				root = big_right(root);
+				root = avl_big_right(root);
 			}
 			break;
 		case -2:
 			if(((root->right->right) ? root->right->right->height : 0) >= ((root->right->left) ? root->right->left->height : 0)) {
-				root = left(root);
+				root = avl_left(root);
 			}
 			else {
-				root = big_left(root);
+				root = avl_big_left(root);
 			}
 			break;
 		default:
@@ -110,12 +64,6 @@ avl_tree* avl_rotation (avl_tree* root) {
 	return root;
 }
 
-/**
- * добавление элемента в АВЛ дерево
- * @param  root    указатель на дерево
- * @param  element элемент для вставки
- * @return         дерево после вставки
- */
 avl_tree* push_in_avl (avl_tree* root, type_tree element) {
 	if (root) {
 		if (root->val > element) {
@@ -134,10 +82,6 @@ avl_tree* push_in_avl (avl_tree* root, type_tree element) {
 	return avl_rotation(root);
 }
 
-/**
- * очистка памяти из под дерева рекурсивным проходом в глубину
- * @param root указатель на дерево
- */
 void depth_free_avl(avl_tree* root) {
 	if (root) {
 		avl_tree* left = root->left;
@@ -146,19 +90,4 @@ void depth_free_avl(avl_tree* root) {
 		depth_free_avl(left);
 		depth_free_avl(right);
 	}
-}
-
-int main() {
-	avl_tree* root = NULL;
-	int N, elem;
-	int i;
-	scanf("%d", &N);
-	for (i = 0; i < N; i++)
-	{
-		scanf("%d", &elem);
-		root = push_in_avl(root, elem);
-	}
-	printf("%d", (root) ? root->height : 0);
-	depth_free_avl(root);
-	return 0;
 }
