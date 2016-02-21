6ce738635746ecffcfc63e60c5ec584cee907935
2ZeroSix_DEXP <2ZeroSix1@gmail.com>
2016-02-21 20:22:58 +0600

    остановился на слиянии деревьев

--- semester_2/lab3_Huffman/huff_func.c
+++ semester_2/lab3_Huffman/huff_func.c
@@ -1,6 +1,90 @@
 #include "huff_func.h"
 
 
-table_type_hf* read_file_hf(FILE* in) {
-	
+table_type_hf* file_table_hf(FILE* in) {
+	table_counter_hf* c;
+	table_type_hf* table = (table_type_hf*)calloc(table_width_hf, sizeof(table_type_hf));
+	while((c = getc(in)) != EOF) {
+		table[c]++;
+	}
+	return table;
 }
+
+queue_hf* push_ord_hf(queue_hf* queue, tree_hf* new) {
+	if(!new) {
+		return NULL;
+	}
+	if(queue) {
+		tree_hf* prev = NULL;
+		tree_hf* cur = queue;
+		while(cur) {
+			if (cur->count < new->count) {
+				prev = cur;
+				cur = cur->next;
+			}
+			else {
+				prev->next = new;
+				new->next = cur;
+			}
+		}
+		if (!cur) {
+			prev->next = new;
+		}
+	}
+	else {
+		queue = new;
+	}
+	return queue;
+}
+
+tree_hf* take_tree_ord_hf(queue_hf** queue) {
+	if(!queue || !(queue*)){
+		return NULL;
+	}
+	tree_hf* tkd = (queue*)->root;
+	queue_hf* tmp = queue*;
+	(queue*) = (queue*)->next;
+	free(tmp);
+	return tkd;
+}
+
+queue_hf* queue_from_table_hf(table_hf* table) {
+	table_counter_hf i;
+	queue_hf* queue = (queue_hf*)calloc(1, sizeof(queue_hf));
+	for (i = 0; i < table_width_hf; i++) {
+		if(table[i]) {
+			tree_hf* new = (tree_hf*)calloc(1, sizeof(tree_hf));
+			new->code = i;
+			new->count = table[i];
+			queue = push_ord_hf(queue, new);
+		}
+	}
+	return queue;
+}
+
+tree_hf* merge_tree_hf(tree_hf* root1, tree_hf* root2) {
+	tree_hf* new = (tree_hf*)calloc(1, sizeof(tree_hf));
+}
+
+tree_hf* tree_from_queue_hf(queue_hf* queue) {
+	if(!queue) {
+		return NULL;
+	}
+	while(queue->next){
+		queue = push_ord_hf(queue, merge_tree_hf(take_tree_ord_hf(&queue),take_tree_ord_hf(&queue)));
+	}
+	return take_tree_ord_hf(&queue);
+}
+
+long long* table_from_tree_hf() {
+	long long* table = (long long*)calloc()
+}
+
+void write_tree_hf(FILE* out, tree_hf* root) {
+
+}
+
+void compress_file_hf(FILE* in, FILE* out, ) {
+
+}
+
--- semester_2/lab3_Huffman/huff_func.h
+++ semester_2/lab3_Huffman/huff_func.h
@@ -1,14 +1,59 @@
 #ifndef _HUFF_FUNC_H
 #define _HUFF_FUNC_H
 
-#define table_type_hf int // тип данных хранящихся в таблице
+#define table_type_hf int // тип таблицы
+#define table_width_hf 256 // кол-во элементов в таблице
+#define table_counter_hf unsigned char // тип необходимый для хранения индекса таблицы
 
+typedef struct _tree_hf{
+  char code;
+  table_type_hf count;
+  struct _tree_hf* left;
+  struct _tree_hf* right;
+}tree_hf;
+
+typedef struct _queue_hf {
+  tree_hf* root;
+  struct _queue_hf* next;
+}queue_hf;
 
 /**
- * таблица кол-ва встреч символов в файле
+ * добавление в очередь с приоритетом (по возрастанию)
+ * @param  head  указатель на голову очереди
+ * @param  code  код символа
+ * @param  count кол-во встреч этого символа
+ * @return       очередь с приоритетом (по возрастанию) с добавленным элементом
+ */
+queue_hf* push_ord_hf(queue_hf* queue, table_type_hf code, table_counter_hf count);
+
+/**
+ * достает хранящееся в голове очереди дерево
+ * @param  head указатель на указатель на очередь
+ * @return      дерево хранящееся в голове очереди
+ * (сдвигает указатель на голову очереди на следующий элемент очереди)
+ */
+tree_hf* take_tree_ord_hf(queue_hf** queue);
+
+/**
+ * таблица частот из файла (таблица кол-ва встреч символов в файле)
  * @param  in указатель на поток
- * @return    массив из 256 элементов (a[i] = кол-во элементов с кодом i)
+ * @return    таблица частот 
+ * (массив из table_width_hf (смотри в #define) элементов (a[i] = кол-во элементов с кодом i)
  */
 table_type_hf* read_file_hf(FILE* in);
+
+/**
+ * получение очереди с приоритетом (по возрастанию) из таблицы частот
+ * @param  table корректная таблица частот
+ * @return       очередь с приоритетом (по возрастанию) (состоит из деревьев единичной высоты)
+ */
+queue_hf* queue_from_table_hf(table_hf* table);
+
+/**
+ * построение дерева из очереди с приоритетом
+ * @param  queue очередь с приоритетом (по возрастанию)
+ * @return       готовое дерево (построенное для алгоритма сжатия хаффмана)
+ */
+tree_hf* tree_from_queue_hf(queue_hf* queue);
 
 #endif
