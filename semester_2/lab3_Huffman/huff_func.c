8fb236d0477cf7fe74b64eb1ee1c4c8863b2d16c
2ZeroSix_DEXP <2ZeroSix1@gmail.com>
2016-02-28 19:08:41 +0600

    проходит 15/36

--- semester_2/lab3_Huffman/huff_func.c
+++ semester_2/lab3_Huffman/huff_func.c
@@ -111,7 +111,7 @@ void depth_table_hf(tree_hf* root, sym_code* table, sym_code cur) {
     return;
   }
   if(isleaf_hf(root)) {
-	printf("%c %u: code: %llu, %u\n", root->code, root->code, cur.code, cur.bts);
+	// printf("%c %u: code: %llu, %u\n", root->code, root->code, cur.code, cur.bts);
   	table[root->code] = cur;
   }
   cur.code = cur.code << 1;
@@ -143,10 +143,11 @@ sym_code* table_from_tree_hf(tree_hf* root) {
 void write_code_hf(FILE* out, sym_code cur) {
 	static sym_code_code_hf tmp = 0;
 	static sym_code_bts_hf pos = 0;
+	// printf("w_c: %llu, %u, %c. 	\n", cur.code, cur.bts, cur.code);
 	if (cur.bts) {
 		if (pos + cur.bts >= sym_code_MAXbts_hf) {
 			tmp = ((tmp << (sym_code_MAXbts_hf - pos)) + (cur.code >> (cur.bts - sym_code_MAXbts_hf + pos)));
-			fwrite(&tmp, 1, sizeof(sym_code_code_hf), out);
+			fwrite(&tmp, sizeof(sym_code_code_hf), 1, out);
 			// printf("1: tmp: %llu; pos: %u; cur.code: %llu; cur.bts: %u\t", tmp, pos, cur.code, cur.bts);
 			cur.bts -= sym_code_MAXbts_hf - pos;
 			// printf("2: tmp: %llu; pos: %u; cur.code: %llu; cur.bts: %u\t", tmp, pos, cur.code, cur.bts);
@@ -155,14 +156,16 @@ void write_code_hf(FILE* out, sym_code cur) {
 			pos = 0;
 			tmp = 0;
 		}
-		tmp = (tmp << cur.bts) + cur.code;
+		tmp = (tmp << cur.bts) | cur.code;
 		pos += cur.bts;
 		// printf("4: tmp: %llu; pos: %u; cur.code: %llu; cur.bts: %u\n", tmp, pos, cur.code, cur.bts);
 	}
 	else {
 		if (pos) {
+			// printf("5: tmp: %llu; pos: %u; cur.code: %llu; cur.bts: %u\n", tmp, pos, cur.code, cur.bts);
 			tmp = (tmp << (sym_code_MAXbts_hf - pos));
-			fwrite(&tmp, 1, sizeof(sym_code_code_hf), out);
+			// printf("5: tmp: %llu; pos: %u; cur.code: %llu; cur.bts: %u\n", tmp, pos, cur.code, cur.bts);
+			fwrite(&tmp, sizeof(sym_code_code_hf), 1, out);
 		}
 	}
 }
@@ -173,24 +176,23 @@ void write_count_hf(FILE* out, table_type_hf* table_in, sym_code* table_out) {
 	for (i = 0; i < table_width_hf; i++) {
 		count+=table_in[i] * (table_out[i]).bts;
 	}
-	printf("Count: %llu", count);
-	fwrite(&count, 1, sizeof(unsigned long long), out);
+	// printf("Count: %llu\n", count);
+	fwrite(&count, sizeof(unsigned long long), 1, out);
 }
 
 void write_tree_hf(FILE* out, tree_hf* root) {
-	printf("write_tree_hf\n");
 	if(!isleaf_hf(root)){
-		static sym_code tmp = {0,1};
+		sym_code tmp = {0,1};
 		write_code_hf(out, tmp);
 		write_tree_hf(out, root->left);
 		write_tree_hf(out, root->right);
 	}
 	else {
-		static sym_code tmp = {1,1};
+		sym_code tmp = {1,1};
 		write_code_hf(out, tmp);
 		tmp.code = root->code;
 		tmp.bts = 8;
-		printf("%u: %c\n", tmp.code, tmp.code);
+		// printf("w_t_h: %u: %c\n", tmp.code, tmp.code);
 		write_code_hf(out, tmp);
 	}
 }
@@ -199,24 +201,33 @@ void compress_file_hf(FILE* in, FILE* out, sym_code* table) {
 	unsigned char c;
 	sym_code end = {0,0};
 	while (fread(&c, sizeof(unsigned char), 1, in)) {
-		write_code_hf(out, table[(unsigned char)c]);
+		write_code_hf(out, table[c]);
 	}
 	write_code_hf(out, end);
 }
 
 void complete_compress_hf(FILE*in, FILE* out){
-	table_type_hf* tmp_table_in = file_table_hf(in);
-	queue_hf* tmp_queue = queue_from_table_hf(tmp_table_in);
-	tree_hf* tmp_tree = tree_from_queue_hf(tmp_queue);
-	sym_code* tmp_table_out = table_from_tree_hf(tmp_tree);
-	// free_queue_hf(tmp_queue); // сделать
-	write_count_hf(out, tmp_table_in, tmp_table_out);
-	free(tmp_table_in);
-	write_tree_hf(out, tmp_tree);
-	// depth_tree_free_hf(tmp_tree); //сделать
-	_fseeki64(in, 0, SEEK_SET);
-	compress_file_hf(in, out, tmp_table_out);
-	free(tmp_table_out);
+	table_type_hf* tmp_table_in = NULL;
+	queue_hf* tmp_queue = NULL;
+	tmp_table_in = file_table_hf(in);
+	if(tmp_queue = queue_from_table_hf(tmp_table_in)){
+		tree_hf* tmp_tree = NULL;
+		sym_code* tmp_table_out = NULL;
+		tmp_tree = tree_from_queue_hf(tmp_queue);
+		tmp_table_out = table_from_tree_hf(tmp_tree);
+		// free_queue_hf(tmp_queue); // сделать
+		write_count_hf(out, tmp_table_in, tmp_table_out);
+		free(tmp_table_in);
+		write_tree_hf(out, tmp_tree);
+		// depth_tree_free_hf(tmp_tree); //сделать
+		_fseeki64(in, 3, SEEK_SET);
+		compress_file_hf(in, out, tmp_table_out);
+		free(tmp_table_out);
+	}
+	else {
+		// free_queue_hf(tmp_queue); // сделать
+		free(tmp_table_in);		
+	}
 	return;
 }
 
@@ -228,6 +239,7 @@ unsigned char read_bit_hf(FILE* in) {
 		fread(&buf_dec_hf, sizeof(unsigned long long), 1, in);
 		pos_dec_hf = 0;
 	}
+	// printf("r_bit: %u\n", ((buf_dec_hf >> (63 - pos_dec_hf)) % 2));
 	return ((buf_dec_hf >> (63 - pos_dec_hf++)) % 2);
 }
 
@@ -237,15 +249,18 @@ unsigned char read_byte_hf(FILE* in) {
 	for (i = 0; i < 8; i++) {
 		c = (c << 1) | read_bit_hf(in);
 	}
+	// printf("r_byte: %u, %c.\n", c, c);
 	return c;
 }
 
 tree_hf* tree_from_file_hf(FILE* in) {
-	tree_hf* root = (tree_hf*)calloc(1, sizeof(tree_hf));
+	tree_hf* root = NULL;
 	if(read_bit_hf(in)) {
+		root = (tree_hf*)calloc(1, sizeof(tree_hf));
 		root->code = read_byte_hf(in);
 	}
 	else {
+		root = (tree_hf*)calloc(1, sizeof(tree_hf));
 		root->left = tree_from_file_hf(in);
 		root->right = tree_from_file_hf(in);
 	}
@@ -256,33 +271,30 @@ void decompress_file_hf(FILE* in, FILE* out, tree_hf* root, unsigned long long c
 	unsigned long long i;
 	tree_hf* cur = root;
 	for (i = 0; i < count; i++) {
+		if (isleaf_hf(cur)) {
+			fwrite(&(cur->code), sizeof(unsigned char), 1, out);
+			cur = root;
+		}
 		if(read_bit_hf(in)) {
 			if(cur->right)	{
 				cur = cur->right;
-			}
-			else {
-//				printf("%c", cur->code);
-				fwrite(&(cur->code), sizeof(sym_code), 1, out);
-				cur = (root->right) ? root->right : root;
 			}
 		}
 		else {
 			if (cur->left) {
 				cur = cur->left;
 			}
-			else {
-//				printf("%c", cur->code);
-				fwrite(&(cur->code), sizeof(sym_code), 1, in);
-				cur = (root->left) ? root->left : root;
-			}
 		}
+	}
+	if (isleaf_hf(cur) && !isleaf_hf(root)) {
+		fwrite(&(cur->code), sizeof(unsigned char), 1, out);
 	}
 }
 
 void complete_decompres_hf(FILE* in, FILE* out) {
 	tree_hf* root;
 	unsigned long long count;
-	fread(&count, sizeof(unsigned long long), 1, in);
+	if(!fread(&count, sizeof(unsigned long long), 1, in)) return;
 	root = tree_from_file_hf(in);
 	decompress_file_hf(in, out, root, count);
 	// depth_tree_free_hf(tmp_tree); //сделать
--- semester_2/lab3_Huffman/main_huff.c
+++ semester_2/lab3_Huffman/main_huff.c
@@ -1,16 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "huff_func.h"
-
+#include <string.h>
 int main() {
-	FILE* in = fopen("in.txt", "rb");
-	FILE* out = fopen("out", "wb");
-	complete_compress_hf(in , out);
-	fclose(in);
-	fclose(out);
-	in = fopen("out", "rb");
-	out = fopen("test.txt", "wb");
-	complete_decompres_hf(in, out);
-	fclose(in);
-	fclose(out);
+	char c;
+	if ((c = getchar()) == 'c') {
+		getchar();
+		complete_compress_hf(stdin, stdout);
+	}
+	else if (c == 'd') {
+		getchar();
+		complete_decompres_hf(stdin, stdout);
+	}
 	return 0;
 }
