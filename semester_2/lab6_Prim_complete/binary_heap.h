67fc1e67ae3aff80909c396a0ce4c5cb3393ea65
2ZeroSix_DEXP <2ZeroSix1@gmail.com>
2016-04-02 01:30:46 +0600

    binary heap started

--- semester_2/lab6_Prim_complete/binary_heap.h
+++ semester_2/lab6_Prim_complete/binary_heap.h
@@ -1,12 +1,25 @@
 #ifndef _BINARY_HEAP_
-	#define _BINARY_HEAP_	
+	#define _BINARY_HEAP_
+	typedef _heap {
+		void* array; // указатель на область памяти выделенную под кучу
+		size_t count; // текущее кол-во элементов в куче
+		size_t countMAX; // размер памяти выделенной под кучу
+		size_t size; // размер одного элемента
+		int (*comparator)(const void*, const void*); // функция сравнения элементов кучи
+	}heap;
 
+	heap* make_heap(size_t count, size_t size);
 
-	typedef _bh_que{
-		short a;
-		int weight;
-	} bh_que;
+	heap* build_heap(void* array, size_t count, size_t size);
 	
-	void bh_add();
+	void insert(heap* bheap, void* new);
+
+	void* get_max(heap* bheap);
+
+	void* extract_max(heap* bheap);
+
+	void* increase_key(heap* bheap, size_t);
+
+	char isEmpty(heap* bheap);
 
 #endif
