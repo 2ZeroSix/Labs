a92f33790b9a3fefd0bb968f94004f5653ac295b
2ZeroSix <2ZeroSix1@gmail.com>
2016-05-05 00:18:21 +0600

    почти готово
    решил сначала реализовать версию с индексом

--- semester_2/lab6_Prim_complete/binary_heap.c
+++ semester_2/lab6_Prim_complete/binary_heap.c
@@ -1,20 +1,7 @@
 #include "binary_heap.h"
 
-void swap_heap(heap* bheap, size_t a, size_t b) {
-	swap((char*)(bheap->array) + a*(bheap->size), (char*)(bheap->array) + b*(bheap->size), bheap->size);
-	swap(&index[a], &index[b], sizeof(size_t));
-}
-
+#if  _BINARY_HEAP_LOW_MEM_
 void swap(void *a, void *b, size_t size) {
-  void* tmp;
-  tmp = malloc(size);
-  memcpy(tmp, a, size);
-  memcpy(a, b, size);
-  memcpy(b, tmp, size);
-  free(tmp);
-}
-
-void swap_low_mem(void *a, void *b, size_t size) {
   size_t i;
   for (i = 0; i < size; i++) {
 	  char tmp;
@@ -23,6 +10,21 @@ void swap_low_mem(void *a, void *b, size_t size) {
     *((char*) b + i) = tmp;
   }
 }
+#else
+void swap(void *a, void *b, size_t size) {
+  void* tmp;
+  tmp = malloc(size);
+  memcpy(tmp, a, size);
+  memcpy(a, b, size);
+  memcpy(b, tmp, size);
+  free(tmp);
+}
+#endif
+
+void swap_heap(heap* bheap, size_t a, size_t b) {
+	swap((char*)(bheap->array) + a*(bheap->size), (char*)(bheap->array) + b*(bheap->size), bheap->size);
+	swap(&index[a], &index[b], sizeof(size_t));
+}
 
 int compare(heap* bheap, size_t a, size_t b) {
 	return comparator((char*)bheap->array + a*bheap->size, (char*)bheap->array + b*bheap->size);
@@ -30,7 +32,7 @@ int compare(heap* bheap, size_t a, size_t b) {
 
 heap* make_heap(size_t countMAX, size_t size, int (*comparator)(const void*, const void*)) {
 	if(countMAX && size && comparator) {
-		heap* bheap = (heap*)calloc(heap, 1);
+		heap* bheap = (heap*)calloc(1, sizeof(heap));
 		bheap->array = calloc(countMAX, size);
 		bheap->index = calloc(countMAX, sizeof(size_t));
 		bheap->size = size;
@@ -49,7 +51,7 @@ void heapify(heap* bheap, size_t i) {
 	  size_t leftChild = 2 * i + 1;
 	  size_t rightChild = 2 * i + 2;
 	  size_t largestChild = i;
-    
+
     if (leftChild < heapSize && (compare(bheap, leftChild, largestChild)) > 0) {
       largestChild = leftChild;
     }
@@ -64,16 +66,26 @@ void heapify(heap* bheap, size_t i) {
   }
 }
 
-heap* build_heap(void* array, size_t count, size_t size, int (*comparator)(const void*, const void*)) {
-	heap* bheap = make_heapheap(count, size, comparator);
-	if(bheap) {
-		bheap->array = array;
+heap* build_heap(void* array, size_t count, size_t countMAX, size_t size, int (*comparator)(const void*, const void*)) {
+	if (array && count && countMAX && size && comparator) {
 		size_t i;
+		heap* bheap = (heap*)calloc(1, sizeof(heap));
+		bheap->array = array;
+		bheap->index = malloc(countMAX*sizeof(size_t));
+		for(i = 0; i < count; i++) {
+			bheap->index[i] = i;
+		}	
+		bheap->size = size;
+		bheap->count = count;
+		bheap->countMAX = countMAX;
+		bheap->comparator = comparator;
+		
 		for(i = count / 2; i > 0; i--)
 			heapify(bheap, i);
 		heapify(bheap, i);
+		return bheap;
 	}
-	return bheap;
+	return NULL;
 }
 
 char insert(heap* bheap, void* new) {
@@ -81,10 +93,10 @@ char insert(heap* bheap, void* new) {
 	size_t cur = bheap->count;
 	size_t size = bheap->size;
 	size_t parent = (cur - 1) / 2;
-
 	if(cur >= bheap->countMAX) {
 		return 0;
 	}
+	bheap->index[bheap->count] = bheap->count;
 	memcpy((char*)array + cur*size, new, size);
 	while(cur && (compare(bheap, cur, parent) > 0)) {
 		swap_heap(bheap, cur, parent);
@@ -96,23 +108,44 @@ char insert(heap* bheap, void* new) {
 }
 
 void* get_max(heap* bheap) {
-	swap(bheap->array, (char*)(bheap->array) + bheap->count, bheap->size);
+	if (isEmpty(bheap)) {
+		return NULL;
+	}
+	swap_heap(bheap, 0, bheap->count);
 	bheap->count--;
-	return (char*)(bheap->array) + (bheap->count + 1);
+	heapify(bheap, 0);
+	return (char*)(bheap->array) + (bheap->count * bheap->size);
 }
 
 void* extract_max(heap* bheap) {
 	if (isEmpty(bheap)) {
 		return NULL;
 	}
-	void* result = calloc(bheap->size);
+	void* result = malloc(bheap->size);
 	memcpy(result, bheap->array, bheap->size);
-	memcpy(bheap->array, (char*)(bheap->array) + bheap->count, bheap->size);
+	swap_heap(bheap, 0, bheap->count);
 	bheap->count--;
 	heapify(bheap, 0);
 	return result;
 }
 
 char isEmpty(heap* bheap) {
-	return bheap->count;
+	return ((bheap->count) ? 0 : 1);
 }
+
+void update_by_index(heap* bheap, size_t id, void* upd) {
+	void* array = bheap->array;
+	size_t cur = bheap->index[id];
+	size_t size = bheap->size;
+	if (bheap->comparator(((char*)array + (cur * size)), (upd), (size))) {
+		memcpy(((char*)array + (cur * size)), (upd), (size));
+		heapify(bheap, cur);
+	}
+	else {
+		memcpy(((char*)array + (index[id] * size)), (upd), (size));
+		//TODO дописать поднятие элемента вверх
+		while(cur > 0	)
+	}
+}
+
+void* 
--- semester_2/lab6_Prim_complete/binary_heap.h
+++ semester_2/lab6_Prim_complete/binary_heap.h
@@ -1,7 +1,9 @@
 #ifndef _BINARY_HEAP_
 	#define _BINARY_HEAP_
+	#define _BINARY_HEAP_LOW_MEM_ 0
 	typedef _heap {
 		void* array; // указатель на область памяти выделенную под кучу
+		//TODO придумать универсальную замену по ключу (новые элементы индексируются неверно после удаления элементов из кучи, нет проверки на наличия элемента с данным индексом в массиве)
 		size_t* index; // текущее расположение элементов (в позиции n хранится положение n-ного добавленного элемента)
 		size_t count; // текущее кол-во элементов в куче
 		size_t countMAX; // размер памяти выделенной под кучу
@@ -9,18 +11,63 @@
 		int (*comparator)(const void*, const void*); // функция сравнения элементов кучи
 	}heap;
 
-	heap* make_heap(size_t count, size_t size, int (*comparator)(const void*, const void*));
+	/**
+	 * создание пустой структуры "бинарная куча"(выделяется)
+	 * @param  countMAX 	максимальное кол-во элементов
+	 * @param  size  			размер одного элемента
+	 * @param  comparator	функция сравнения (strcmp подобные)
+	 * @return      			указатель на структуру "бинарная куча", если каждый из параметров ненулевой
+	 *                    NULL	иначе
+	 */
+	heap* make_heap(size_t countMAX, size_t size, int (*comparator)(const void*, const void*));
 
-	heap* build_heap(void* array, size_t count, size_t size, int (*comparator)(const void*, const void*));
+	/**
+	 * Построение бинарной кучи на массиве
+	 * @param		array				указатель массив, который нужно перестроить в бинарную кучу
+	 *                  		(при использовании этого массива не через функционал кучи, свойства кучи могут нарушиться)
+	 * @param		count				кол-во элементов в массиве
+	 * @param		countMAX		кол-во элементов, которое может поместиться в массиве
+	 * @param		size				размер элемента массива
+	 * @param		comparator	функция сравнения (strcmp подобные)
+	 * @return							указатель на структуру "бинарная куча", если каждый из параметров ненулевой
+	 *                      NULL	иначе
+	 */
+	heap* build_heap(void* array, size_t count, size_t countMAX, size_t size, int (*comparator)(const void*, const void*));
 	
-	void insert(heap* bheap, void* new);
+	/**
+	 * добавление элемента в кучу
+	 * @param 	bheap указатель на бинарную кучу
+	 * @param 	new   указатель на новый элемент
+	 * @return				1 если получилось добавить
+	 *                0 иначе
+	 */
+	char insert(heap* bheap, void* new);
 
+	/**
+	 * удаление максимального элемента
+	 * @param 	bheap указатель на бинарную кучу
+	 * @return				указатель на максимальный элемент внутри кучи (будет правильным до следующего insert), если куча не пуста
+	 *                NULL	иначе
+	 */
 	void* get_max(heap* bheap);
 
+	/**
+	 * удаление максимального элемента
+	 * @param 	bheap указатель на бинарную кучу
+	 * @return				указатель на максимальный элемент (скопированный в новую выделенную память), если куча не пуста
+	 *                NULL	иначе
+	 */
 	void* extract_max(heap* bheap);
 
-	void* increase_key(heap* bheap, size_t);
-
+	/**
+	 * проверка кучи на пустоту
+	 * @param  bheap указатель на бинарную кучу
+	 * @return       1 если пуста
+	 *               0 иначе
+	 */
 	char isEmpty(heap* bheap);
+
+	//TODO замена по индексу
+
 
 #endif
